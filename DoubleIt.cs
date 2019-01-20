716853cdf1e6383ebf3e7a2c40b4720b118d3e05
Brent Perry <contact@brentallan.me>
2019-01-20 11:31:54 -0600

    initialize repo

---
+++ DoubleIt.cs
@@ -0,0 +1,10 @@
+using System;
+
+class DoubleIt
+{
+    static void Main()
+    {
+        Console.WriteLine("Give me a number, and I will double it for you!");
+        int yourNumber = Console.ReadLine();
+    }
+}
